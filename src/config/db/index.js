a1a3cfafeefb11408a59d597d30c5e4496b15961
KhangThinh0331 <khangthinh0331@gmail.com>
2025-12-27 22:40:11 +0700

    update db/index.js

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 async function connect() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log('Connected to database successfully')
     } catch (error) {
         console.log('Failed to connect to database:', error)
