141de4eb47c943b15fe0e9afb134c095fc876438
KhangThinh0331 <khangthinh0331@gmail.com>
2025-12-27 22:13:00 +0700

    set up .env

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/bubble_tea')
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
         console.log('Connected to database successfully')
     } catch (error) {
         console.log('Failed to connect to database:', error)
