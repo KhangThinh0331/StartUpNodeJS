1d37aef5553f6160fb240698231fcc8d7bdbc3c2
KhangThinh0331 <khangthinh0331@gmail.com>
2025-12-27 23:05:57 +0700

    update gitignore

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,6 +3,7 @@ async function connect() {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
         console.log('Connected to database successfully')
+        console.log('Connected DB:', mongoose.connection.name)
     } catch (error) {
         console.log('Failed to connect to database:', error)
     }
